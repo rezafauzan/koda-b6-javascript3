6ed3e8370404de216907f712d7e432dc7c2706ea
Reza Fauzan Adhima <rezafauzan593@gmail.com>
2025-12-30 11:31:57 +0700

    add task2.js contain 5 builtin function

---
+++ task2.js
@@ -0,0 +1,55 @@
+// 5 Built in function
+// 1. parseInt() merupakan fungsi bawaan javascript untuk merubah String menjadi Number
+// Contoh penggunaan:
+function penjumlahan(angka1,angka2){
+    return parseInt(angka1) + parseInt(angka2)
+}
+
+console.log(penjumlahan("40",60))
+
+// 2. isNaN() merupakan fungsi bawaan javascript untuk melakukan pengecekan apakah argument merupakan Not a Number
+// Contoh penggunaan:
+function penjumlahanKhusus(angka1,angka2){
+    if(isNaN(angka1) === true || isNaN(angka2) === true){
+        return `salah satu inputan bukanlah sebuah angka "${angka1}" + ${angka2}`
+    }else{
+        return parseInt(angka1) + parseInt(angka2)
+    }
+}
+
+console.log(penjumlahanKhusus("Nama Saya adalah broo",100))
+
+// 3. isFinite() merupakan fungsi bawaan javascript untuk melakukan pengecekan apakah argument merupakan bilangan yang tidak NaN dan tidak infinite
+// Contoh penggunaan:
+
+function pembagian(angka1,angka2){
+    if(isNaN(angka1) === true || isNaN(angka2) === true){
+        return `salah satu inputan bukanlah sebuah angka "${angka1}" + ${angka2}`
+    }else{
+        let hasil = parseInt(angka1) / parseInt(angka2)
+        if(isFinite(hasil) !== false){
+            return hasil
+        }else{
+            return "Pembagian menghasilkan angka tak terbatas"
+        }
+    }
+}
+
+console.log(pembagian(1000000,0))
+
+// 4. escape() merupakan fungsi bawaan javascript untuk merubah seluruh karakter dengan escape
+// Contoh penggunaan:
+
+function kabur(kata){
+    return escape(kata)
+}
+
+console.log(kabur("it's a good day to start learn <HTML 5>"))
+// 5. unescape() merupakan fungsi bawaan javascript untuk merubah seluruh karakter escape menjadi karakter biasa
+// Contoh penggunaan:
+
+function gakabur(kata){
+    return unescape(kata)
+}
+
+console.log(gakabur("it%27s%20a%20good%20day%20to%20start%20learn%20%3CHTML%205%3E"))
