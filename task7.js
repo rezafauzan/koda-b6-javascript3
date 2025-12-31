fdf8353ebeff129f780cc8cdeb8168985edc1f36
Reza Fauzan Adhima <rezafauzan593@gmail.com>
2025-12-31 16:16:34 +0700

    add task7.js to show name from data with setTimeout using promise

---
+++ task7.js
@@ -0,0 +1,65 @@
+let data = [
+    {
+        name: 'John',
+        time: 1500
+    },
+    {
+        name: 'Ed',
+        time: 2200
+    },
+    {
+        name: 'Jane',
+        time: 500
+    },
+    {
+        name: 'Ujang',
+        time: 800
+    },
+]
+
+function antri(data){
+    let antrian = 0
+    function mulaiAntri(orang){
+        return new Promise(
+            (resolve) => {
+                let orang = data[antrian]
+                if(orang){
+                    setTimeout(
+                        ()=>{
+                            console.log(orang.name)
+                            antrian += 1
+                            resolve(mulaiAntri())
+                        },
+                        orang.time)
+                }
+            }
+        )
+    }
+    return mulaiAntri()
+}
+
+
+antri(data).then((success)=>{console.log(success)})
+function antri(data){
+    let antrian = 0
+    function mulaiAntri(orang){
+        return new Promise(
+            (resolve) => {
+                let orang = data[antrian]
+                if(orang){
+                    setTimeout(
+                        ()=>{
+                            console.log(orang.name)
+                            antrian += 1
+                            resolve(mulaiAntri())
+                        },
+                        orang.time)
+                }
+            }
+        )
+    }
+    return mulaiAntri()
+}
+
+
+antri(data).then((success)=>{console.log(success)})
