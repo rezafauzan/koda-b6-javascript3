38c4afe5997e931059a1e4cd6a237e1ce58b8472
Reza Fauzan Adhima <rezafauzan593@gmail.com>
2025-12-31 09:48:33 +0700

    add task4.js a program to handle queue with setTimeout(Asynchronous Practice)

---
+++ task4.js
@@ -0,0 +1,51 @@
+// program antri
+const data = [
+    {
+        nama: "Juli",
+        wait: 4,
+    },
+    {
+        nama: "Julaini",
+        wait: 7,
+    },
+    {
+        nama: "Julian",
+        wait: 9,
+    },
+    {
+        nama: "Julianti",
+        wait: 4,
+    },
+    {
+        nama: "Juliana",
+        wait: 6,
+    },
+    {
+        nama: "Julianto",
+        wait: 9,
+    },
+    {
+        nama: "alJuli",
+        wait: 9,
+    }
+]
+// buat program antri sesuai waktu tunggu nya
+// output info menunggu antrian
+// setelah waktu tunggu buat notif pesanan punya nama sudah selesai
+// output info menunggu antrian
+
+function pesanMakanan(nama,waktu){
+    console.log(`\n****Halo ${nama} silahkan untuk Menunggu antriannya ya selama ${waktu} detik****\n`)
+    setTimeout(
+        ()=>{
+            console.log(`\n****Halo ${nama} pesananmu sudah siap! silahkan untuk menuju pengambilan****\n`)
+        },
+        waktu * 1000
+    )
+}
+
+data.forEach(
+    (pemesan) => {
+        pesanMakanan(pemesan.nama,pemesan.wait)
+    }
+)
